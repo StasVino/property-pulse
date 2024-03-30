7fb08e3fb1d27d89554faae25543765812e49092
unknown <st13vv@gmail.com>
2024-03-30 19:46:40 +0300

    practice

---
+++ app/properties/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Link from "next/link";
+
+const propertiesPage = () => {
+  <div>
+    <h1 className="text-3x1">Properties</h1>
+    <Link href="/properties">Go home</Link>
+  </div>;
+};
+
+export default propertiesPage;
