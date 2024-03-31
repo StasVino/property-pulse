000a685533e92d21ba58888ae2a4e78622dc7c4a
unknown <st13vv@gmail.com>
2024-03-31 20:49:44 +0300

    practice

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/navigation";
+import { userAgent } from "next/server";
+
+import React from "react";
+
+const page = () => {
+  console.log("hello");
+  return <div>PropertyPage</div>;
+};
+
+export default page;
