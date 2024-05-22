27ffbc0e64abb61898c1274eaebd1e8fa2feffd9
unknown <st13vv@gmail.com>
2024-05-22 20:52:37 +0300

    added mapbox and geocoding

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -6,6 +6,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from 'react-icons/fa';
+import PropertyMap from './PropertyMap';
 
 export const PropertyDetails = ({ property }) => {
   return (
@@ -92,7 +93,7 @@ export const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />{' '}
       </div>
     </main>
   );
