4f1bb77cd6c57214615ebecae57686332062de84
unknown <st13vv@gmail.com>
2024-06-05 21:30:54 +0300

    added display to search

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -2,6 +2,11 @@
 
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { FaArrowAltCircleLeft } from 'react-icons/fa';
+import PropertyCard from '@/components/PropertyCard';
+import Spinner from '@/components/Spinner';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 const SearchResultsPage = () => {
   const [properties, setProperties] = useState([]);
@@ -19,7 +24,7 @@ const SearchResultsPage = () => {
           `/api/properties/search?location=${location}&propertyType=${propertyType}`
         );
 
-        if (res === 200) {
+        if (res.status === 200) {
           const data = await res.json();
           setProperties(data);
         } else {
@@ -33,8 +38,40 @@ const SearchResultsPage = () => {
     };
     fetchSearchResults();
   }, [location, propertyType]);
-
-  return <div>SearchResultsPage</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col item-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className="px-4 py-6">
+          <div className="container-xl lg:container m-auto px-4 py-6">
+            <Link
+              href="/properties"
+              className="flex itmes-center text-blue-500 hover:underline mb-3"
+            >
+              <FaArrowAltCircleLeft className="mr-2 mb-1" />
+              Back To Properties
+            </Link>
+            <h1 className="text-2xl mb-4">Search Results</h1>
+            {properties.length === 0 ? (
+              <p>No search results</p>
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
+          </div>{' '}
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SearchResultsPage;
