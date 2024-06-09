2eac03176e6040e4ea60a8a2af2cf3f99bc03071
unknown <st13vv@gmail.com>
2024-06-09 21:03:07 +0300

    added messaging display

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -21,8 +21,8 @@ export const GET = async (request) => {
 
     // Find message in database
     const messages = await Message.find({ recipient: userId })
-      .populate('sender', 'name')
-      .populate('property', 'title');
+      .populate('sender', 'username')
+      .populate('property', 'name');
 
     return new Response(JSON.stringify(messages), {
       status: 200,
