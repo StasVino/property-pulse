36afe86cbc489fca1c4e540ed5cd40030bbe4491
unknown <st13vv@gmail.com>
2024-06-17 22:34:42 +0300

    moved properties to components

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,5 @@
-import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
+import Properties from '@/components/Properties';
 import PropertySearchForm from '@/components/PropertySearchForm';
 
 const PropertiesPage = async () => {
@@ -15,20 +15,7 @@ const PropertiesPage = async () => {
           <PropertySearchForm />
         </div>
       </section>
-
-      <section className="px-4 py-6">
-        <div className="container-xl lg:container m-auto px-4 py-6">
-          {properties.length === 0 ? (
-            <p>No properies found</p>
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {properties.map((property) => (
-                <PropertyCard key={property._id} property={property} />
-              ))}
-            </div>
-          )}
-        </div>{' '}
-      </section>
+      <Properties />
     </>
   );
 };
